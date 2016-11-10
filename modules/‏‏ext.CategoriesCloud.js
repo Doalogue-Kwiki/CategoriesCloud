e028c19f4c7e0a3c1db1fa4dcc37c60fea3917d5
HagaiAS <hagai.asaban@gmail.com>
2016-11-10 14:58:30 +0200

    WIP

--- modules/‏‏ext.CategoriesCloud.js
+++ modules/‏‏ext.CategoriesCloud.js
@@ -1,5 +1,5 @@
 /**
- * þþext.AdvancedSearchBox
+ * þþext.CategoriesCloud
  *
  * @author Hagai Asaban
  * @license MIT
@@ -7,79 +7,24 @@
 
 ( function ( mw, $ ) {
 
-	// Compute form data for search suggestions functionality.
-	function computeResultRenderCache( context ) {
-		var $form, baseHref, linkParams;
-
-		// Compute common parameters for links' hrefs
-		$form = context.config.$region.closest( 'form' );
-
-		baseHref = $form.attr( 'action' );
-		baseHref += baseHref.indexOf( '?' ) > -1 ? '&' : '?';
-
-		linkParams = {};
-		$.each( $form.serializeArray(), function ( idx, obj ) {
-			linkParams[ obj.name ] = obj.value;
-		} );
-
-		return {
-			textParam: context.data.$textbox.attr( 'name' ),
-			linkParams: linkParams,
-			baseHref: baseHref
-		};
-	}
-
-	// The function used to render the suggestions.
-	function customRenderFunction( text, context ) {
-		var page, namespace,
-			title = mw.Title.newFromText( text ),
-			info = computeResultRenderCache( context );
-
-		info.linkParams[ info.textParam ] = text;
-
-		page = title.getMainText();
-		namespace = $( '<span>' )
-			.text( mw.config.get( 'wgFormattedNamespaces' )[ title.namespace ] )
-			.addClass( 'mw-mnss-srcc' );
-
-		// 'this' is the container <div>, jQueryfied
-		this
-			.append( page, namespace )
-			.wrap(
-				$( '<a>' )
-					.attr( 'href', info.baseHref + $.param( info.linkParams ) )
-					.addClass( 'mw-searchSuggest-link' )
-			);
-	}
-
-	function loadAdvancedSearchBox() {
-		//var $searchInput = $( '#AdvancedSearchBox' );
-		var $searchInput = $( '#searchInput' );
+	function loadCategoriesCloud() {
 		
-		$searchInput.suggestions( {
-			fetch: function ( query ) {
-				var $el;
-				var allNamespaces = mw.config.get( 'wgContentNamespaces' ).join( '|' );
-				if ( query.length !== 0 ) {
-					$el = $( this );
-					$el.data( 'request', ( new mw.Api() ).get( {
-						action: 'opensearch',
-						search: query,
-						namespace: allNamespaces,
-						suggest: ''
-					} ).done( function ( data ) {
-						$el.suggestions( 'suggestions', data[1] );
-					} ) );
-				}
-			},
-			result: {
-				render: customRenderFunction
-			}
+		var list = [['foo', 12], ['bar', 6]];
+		
+		WordCloud(document.getElementById('categoriesCloudCanvas'), { 
+			list: list,
+			gridSize: 18,
+			weightFactor: 3,
+			color: '#001000',
+			hover: window.drawBox,
+			click: function(item) {
+				alert(item[0] + ': ' + item[1]);
+			}			
 		} );
 	};
 
 	$( function () {
-        loadAdvancedSearchBox();
+        loadCategoriesCloud();
     });
 	
 }( mediaWiki, jQuery ) );
